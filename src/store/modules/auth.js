2da9e4991b1c8e7cbdf46097cc56d6c55ca8d15c
njerucyrus <njerucyrus5@gmail.com>
2018-03-22 19:29:56 +0300

    added api endpoint configuration file

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,6 +1,4 @@
-import axios from 'axios'
-
-const api = axios.create({baseURL: 'http://localhost:8000/api/'})
+import api from '../api'
 
 const state = {
   isLoggedIn: false,
--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -1,7 +1,5 @@
-import axios from 'axios'
 
-const api = axios.create({baseURL: 'http://localhost:8000/api'});
-
+import api from '../api'
 const state = {
   productList: [],
   product: {},
